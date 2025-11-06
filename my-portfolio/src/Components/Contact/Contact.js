ddf6ecd3fbdc4449b2b1a959091df870c44a55fb
priya2805-dev <priyarajput79999@gmail.com>
2025-11-06 17:44:01 +0530

    completed form section

--- my-portfolio/src/Components/Contact/Contact.js
+++ my-portfolio/src/Components/Contact/Contact.js
@@ -1,25 +1,32 @@
-import React from 'react'
-import instaIcon from "../../assets/instagram.png"
-
+import React from "react";
+import instaIcon from "../../assets/instagram.png";
+import LinkdinIcon from "../../assets/linkedin.png";
+import GithubIcon from "../../assets/github.png";
+import "./Contact.css"
 const Contact = () => {
   return (
-    <section className='contactPage'>
-        <div id="contact">
-            <h1 className='contactPageTitle'>Contact Me</h1>
-            <span className='contactDesc'>Let’s Build Something Together – Send a Message!</span>
-                <form className='contactForm'>
-                    <input type='text' className='name' placeholder='Your Name'/>
-                    <input type='email' className='email' placeholder='Your Mail'/>
-                    <button type='submit' value="send" className='submitBtn'>Submit</button>
-                    <div className='links'>
-                      <img src={instaIcon} alt='' className='link'/> 
-
-                    </div>
-                    </form>
-        </div>
-      
+    <section className="contactPage">
+      <div id="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDesc">
+          Let’s Build Something Together – Send a Message!
+        </span>
+        <form className="contactForm">
+          <input type="text" className="name" placeholder="Your Name" />
+          <input type="email" className="email" placeholder="Your Email" />
+          <textarea name="message"  className="msg" placeholder="Your Message"  rows="5"/>
+          <button type="submit" value="send" className="submitBtn">
+            Submit
+          </button>
+          <div className="links">
+            <img src={instaIcon} alt="InstaIcon" className="link" />
+            <img src={LinkdinIcon} alt="LinkdinIcon" className="link" />
+            <img src={GithubIcon} alt="GithubIcon" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
